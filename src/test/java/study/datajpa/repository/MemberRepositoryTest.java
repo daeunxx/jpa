1031e3186aebef836cd223633aef1de5e531902b
김다은 <sss3598@gmail.com>
2024-03-04 23:57:00 +0900

    공통인터페이스 테스트케이스 작성

--- src/test/java/study/datajpa/repository/MemberRepositoryTest.java
+++ src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -8,6 +8,10 @@ import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
 import study.datajpa.entity.Member;
 
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 @SpringBootTest
 @Transactional
 @Rollback(value = false)
@@ -18,6 +22,9 @@ class MemberRepositoryTest {
 
     @Test
     public void testMember() {
+
+        System.out.println("memberRepository : " + memberRepository.getClass());
+
         Member member = new Member("tester");
         Member savedMember = memberRepository.save(member);
 
@@ -26,5 +33,36 @@ class MemberRepositoryTest {
         Assertions.assertEquals(findMember.getId(), member.getId());
         Assertions.assertEquals(findMember.getUsername(), member.getUsername());
         Assertions.assertEquals(findMember, member);
+    }
+
+    @Test
+    public void basicCRUD() {
+        Member member1 = new Member("member1");
+        Member member2 = new Member("member2");
+        memberRepository.save(member1);
+        memberRepository.save(member2);
+
+        // 단건 조회 검증
+        Member findMember1 = memberRepository.findById(member1.getId()).get();
+        Member findMember2 = memberRepository.findById(member2.getId()).get();
+        assertEquals(findMember1, member1);
+        assertEquals(findMember2, member2);
+
+        findMember1.setUsername("daeun");
+
+        // 리스트 조회 검증
+        List<Member> members = memberRepository.findAll();
+        assertEquals(members.size(), 2);
+
+        // 카운트 검증
+        long count = memberRepository.count();
+        assertEquals(count, 2);
+
+        // 삭제 검증
+        memberRepository.delete(member1);
+        memberRepository.delete(member2);
+
+        long delteCount = memberRepository.count();
+        assertEquals(delteCount, 0);
     }
 }
