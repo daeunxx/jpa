5757932177664a6a2f526847376aa4712b73e1fe
김다은 <sss3598@gmail.com>
2024-03-09 13:38:14 +0900

    프로젝트 설정 및 영속성 관리

--- src/main/java/JpaMain.java
+++ src/main/java/JpaMain.java
@@ -8,22 +8,22 @@ import java.util.List;
 public class JpaMain {
 
     public static void main(String[] args) {
-        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello-test");
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
         EntityManager em = emf.createEntityManager();
 
         EntityTransaction tx = em.getTransaction();
         tx.begin();
 
         try {
+            Member member = em.find(Member.class, 1L);
+            member.setName("aaa");
 
-            List<Member> result = em.createQuery("select m from Member m", Member.class)
-                    .setFirstResult(1)
-                    .setMaxResults(10)
-                    .getResultList();
-            result.forEach(m -> System.out.println("member : " + m.getName()));
+            em.clear();
+            System.out.println("member = " + member.getName());
 
             tx.commit();
         } catch(Exception e) {
+            e.printStackTrace();
             tx.rollback();
         } finally {
             em.close();
--- src/main/java/Member.java
+++ src/main/java/Member.java
@@ -1,5 +1,6 @@
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -10,4 +11,12 @@ public class Member {
     @Id
     private Long id;
     private String name;
+
+    public Member() {
+    }
+
+    public Member(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
 }
