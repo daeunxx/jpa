de486abcfd4b327ed25f33fb18667b80bea7635b
daeun <sss3598@gmail.com>
2024-08-11 18:49:04 +0900

    환경 설정 | application.yml

--- src/test/java/org/example/querydsl/QuerydslApplicationTests.java
+++ src/test/java/org/example/querydsl/QuerydslApplicationTests.java
@@ -10,10 +10,12 @@ import org.example.querydsl.entity.QHello;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Commit;
 import org.springframework.transaction.annotation.Transactional;
 
 @SpringBootTest
 @Transactional
+@Commit
 class QuerydslApplicationTests {
 
   @Autowired
