4c7a76c226e8d4485936d11682ce2979cd1e7a78
daeun <sss3598@gmail.com>
2024-08-13 20:20:52 +0900

    중급 문법 | Tuple(Repository 안에서만 사용할 것) -> 외부에서는 DTO 사용

--- src/test/java/org/example/querydsl/QuerydslBasicTest.java
+++ src/test/java/org/example/querydsl/QuerydslBasicTest.java
@@ -437,4 +437,28 @@ public class QuerydslBasicTest {
       System.out.println("str = " + str);
     }
   }
+
+  @Test
+  public void simpleProjection() {
+    List<String> result = queryFactory
+        .select(member.username)
+        .from(member)
+        .fetch();
+
+    System.out.println("result = " + result);
+  }
+
+  @Test
+  public void tupleProjection() {
+    List<Tuple> result = queryFactory
+        .select(member.username, member.age)
+        .from(member)
+        .fetch();
+
+    for (Tuple tuple : result) {
+      String username = tuple.get(member.username);
+      Integer age = tuple.get(member.age);
+      System.out.println("username = " + username + ", age = " + age);
+    }
+  }
 }
